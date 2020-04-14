79ee9b21d1a448a30d572b8e6df80dd3e1bdd123
Miguel Campos <camposmiguel@gmail.com>
2020-04-14 13:10:11 +0200

    Se eliminar el valor de API_KEY

--- app/src/main/java/dev/miguelcampos/themoviedbandroid/common/Constantes.kt
+++ app/src/main/java/dev/miguelcampos/themoviedbandroid/common/Constantes.kt
@@ -3,7 +3,7 @@ package dev.miguelcampos.themoviedbandroid.common
 class Constantes {
     companion object {
         public val TMDBAPI_BASE_URL = "https://api.themoviedb.org/3/"
-        public val API_KEY = "433d2c486572afb242c6fe7c1ddc6771"
+        public val API_KEY = "XXXXXXXXXXXXXXXXX"
         public val URL_PARAM_API_KEY = "api_key"
         public val URL_PARAM_LANGUAGE = "language"
         public val IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w500"
